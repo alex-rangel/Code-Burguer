ab6b812eb1ae22884a949f1ed4cc38a7fac6955e
alex-rangel <alexrangel90@gmail.com>
2024-03-07 20:04:10 -0300

    coloquei as variaveis de ambiente no codigo

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,8 @@
 module.exports = {
-    username: "postgres",
-    password: "postgres",
-    database: "codeBurger",
-    host: "localhost",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host:process.env.DB_HOST ,
     dialect: "postgres",
     define: {
       timestamps:true,
